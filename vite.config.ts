330b7ec6575de7c661c56a47d05ca09c5a18e812
Cdeth567 <11kvvkvv11@mail.ru>
2026-04-19 00:17:41 +0300

    add handling 404 error

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Desker/" : "/",
-  server: {
-    host: "::",
-    port: 5173,
-    hmr: {
-      overlay: false,
-    },
-  },
+export default defineConfig({
+  base: "/Desker/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
